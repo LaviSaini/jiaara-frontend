c5ba1ae15bad0ce53d9f5c1b196b63b0bd85af6d
lavisaini1996 <lavisaini1996@gmail.com>
2025-04-17 16:23:20 +0530

    fixing issue

--- src/components/pages/confirm-order/ConfirmOrder.jsx
+++ src/components/pages/confirm-order/ConfirmOrder.jsx
@@ -108,7 +108,7 @@ export default function ConfirmOrder() {
       fetchOrderDetail(orderData)
     }
     return () => {
-      localStorage.removeItem('id')
+      localStorage.removeItem('order-data')
     }
   }, [])
   return (
